031f754f2d9ac19f1fab094c09d576dc7fd42114
Ashish Karn <ashishkrkarn@gmail.com>
2026-02-06 23:12:16 +0530

    javascript closure

--- closures.js
+++ closures.js
@@ -2,7 +2,7 @@
 // from its outer lexical scope. Even after the outer function
 //has finished executing, those variables remain accessible to
 //the inner function. The is commonly used for data privacy,
-//maintaining state and function factories.
+//maintaining state and function factories. 
 function fun() {
   var i = 10;
   return function inner() {
