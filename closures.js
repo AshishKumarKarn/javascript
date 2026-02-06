dd84ff5e7b45e48526efc843abed5c08e489d010
Ashish Karn <ashishkrkarn@gmail.com>
2026-02-06 23:11:33 +0530

    "closure definition"

--- closures.js
+++ closures.js
@@ -1,7 +1,12 @@
-function fun(){
-    var i =10;
-    return function inner(){
-        console.log(i);
-    }
+// A closure is created when an inner function captures variables
+// from its outer lexical scope. Even after the outer function
+//has finished executing, those variables remain accessible to
+//the inner function. The is commonly used for data privacy,
+//maintaining state and function factories.
+function fun() {
+  var i = 10;
+  return function inner() {
+    console.log(i);
+  };
 }
 fun()();
